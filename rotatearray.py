759d6012074cebc52b1591140612ad3a06860756
G GUNAGI <pooja.g.gunagi@accenture.com>
2021-08-05 16:34:31 +0530

    array

---
+++ rotatearray.py
@@ -0,0 +1,17 @@
+def rotate(a,n,d):
+    temp=[]
+    i=0
+    while i<d:
+        temp.append(a[i])
+        i=i+1
+    i=0
+    while d<n:
+        a[i]=a[d]
+        d=d+1
+        i=i+1
+
+    a[:]=a[:i]+temp
+    return a
+
+l=[1,2,3,4,5,6,7]
+print(rotate(l,7,2))
